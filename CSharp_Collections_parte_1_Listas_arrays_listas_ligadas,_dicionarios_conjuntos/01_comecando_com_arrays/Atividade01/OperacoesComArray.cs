9e8c1b6b27f76b4612d6147231f7ba57f16e27ea
Angelotravain <angelotravain96@gmail.com>
2023-03-26 13:18:34 -0300

    Finalizado primeira parte curso de array

---
+++ CSharp_Collections_parte_1_Listas_arrays_listas_ligadas,_dicionarios_conjuntos/01_comecando_com_arrays/Atividade01/OperacoesComArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade01
+{
+    internal class OperacoesComArray
+    {
+        static void Main(string[] args)
+        {
+            /*Nesta aula vamos procurar um elemento dentro do array */
+            string aulaIntro = "Introdução as coleções";
+            string aulaModelando = "Modelando a Classe Aula";
+            string aulaSets = "Trabalhando com Conjuntos";
+
+            string[] aulas = new string[]
+            { aulaIntro,
+              aulaModelando,
+              aulaSets
+            };
+
+            foreach (var aux in aulas){
+                Console.WriteLine($"A aula modelando está no indice {Array.IndexOf(aulas, aulaModelando)}");
+                Console.WriteLine(Array.LastIndexOf(aulas, aulaModelando));
+                Console.WriteLine(Array.LastIndexOf(aulas, aulaModelando));
+
+                // invertendo modifica a ordem do array
+                Array.Reverse(aulas);
+                Console.WriteLine(aulas);
+
+                // modificar o tamanho do array
+
+                Array.Resize(ref aulas, 2);
+
+                Console.WriteLine(aulas);
+            }
+        }
+    }
+}
---
+++ CSharp_Collections_parte_1_Listas_arrays_listas_ligadas,_dicionarios_conjuntos/01_comecando_com_arrays/Atividade01/declarandoArrays.cs
@@ -0,0 +1,53 @@
+﻿
+
+//namespace Atividade01
+//{
+//    class declarandoArrays
+//    {
+//        static void Main(string[] args)
+//        {
+//            /*Coleção Array*/
+
+//            string aulaIntro = "Introdução as coleções";
+//            string aulaModelando = "Modelando a Classe Aula";
+//            string aulaSets = "Trabalhando com Conjuntos";
+
+//            //Maneira especial de declara um array existe no C#
+
+//            string[] aulas = new string[]
+//            { aulaIntro,
+//              aulaModelando, 
+//              aulaSets
+//            };
+
+//            // Indice igual sua posição -1 
+//            // Primeira forma de declara um array quando já sei que elementos ele tem 
+
+//            // A segunda forma é declarar explicitamente a quantidade de itens que vão ter no array
+
+//            string[] aulasExplicitamente = new string[3];
+//            aulasExplicitamente[0] = aulaIntro;
+//            aulasExplicitamente[1] = aulaModelando;
+//            aulasExplicitamente[2] = aulaSets;
+
+//            Console.WriteLine(aulasExplicitamente);
+
+//            // Forma de percorrer o array 
+
+//            foreach (var x in aulasExplicitamente){
+//                Console.WriteLine(x);
+//            }
+
+//            for (var indice = 0; indice < aulasExplicitamente.Length; indice++)
+//            {
+//                Console.WriteLine(aulasExplicitamente [indice]);
+
+//                aulaIntro = "Trabalhando com array";
+
+//                aulasExplicitamente[0] = aulaIntro;
+
+//                Console.WriteLine(aulasExplicitamente[indice]);
+//            }
+//        }
+//    }
+//}
