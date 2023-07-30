3ea53e0f0af69656bffd06571db175f0a2a4ef2a
Angelotravain <angelotravain96@gmail.com>
2023-07-30 01:09:41 -0300

    Inclusão de novos cursos, curso de criação de APIs

---
+++ .NET 6. Crianco uma WEB API/FilmesApi/FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,23 @@
+﻿using FilmesApi.Modesls;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> Filmes = new();
+
+    [HttpPost] // tipo de requisição Http que será usada, no caso a requisição usada pela API para o post que usa  método abaixo, independente do nome do método com esse Http ele faz a validação 
+    public void AdicionaFilme([FromBody] Filme filme)
+    { 
+        // Aqui é onde realizamos os devidos códigos para serem consumidos pelos ambientes externos da API, 
+        Filmes.Add(filme);
+        Console.WriteLine(filme.Duracao);
+        Console.WriteLine(filme.Titulo);
+
+        // Para realizar a chamada da API, como utilizado no postman usamos o caminho da API e em seguida colocamos o nome da controller que queremos chamar 
+        //No caso aqui /filme não importando entre maiusculo e minusculo, o tipo de dados que trafega entre as API's são arquivos de Json
+    }
+}
---
+++ .NET 6. Crianco uma WEB API/FilmesApi/FilmesApi/Models/Filme.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Modesls;
+
+public class Filme
+{
+    // usar Data Annotation para fazer as devidas validações validações para API podem ser feitas por aqui, deixa o código melhor e mais bem estruturado
+    [Required(ErrorMessage = "O Titulo do filme é obrigatório")]
+    [MaxLength(100, ErrorMessage = "O tamanho do titulo não pode exceder 100caracteres")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "O Genero do filme é obrigatório")]
+    [MaxLength(100, ErrorMessage ="O tamanho do genero não pode exceder 100caracteres")]
+    public string Genero { get; set; }
+    [Required]
+    [Range(60,600, ErrorMessage = "A duração deve ser entre 60 e 600 minutos")]
+    public int Duracao { get; set; }
+}
